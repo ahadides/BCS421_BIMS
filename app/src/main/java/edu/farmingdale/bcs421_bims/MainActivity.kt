92fa2f010c9ae0aedc064c1d89ef1c0cc7b9247f
Elan Cheung <elan.cheung@gmail.com>
2023-11-02 19:46:09 -0400

    New project commit

---
+++ app/src/main/java/edu/farmingdale/bcs421_bims/MainActivity.kt
@@ -0,0 +1,11 @@
+package edu.farmingdale.bcs421_bims
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "BCS421_BIMS"
+include(":app")
