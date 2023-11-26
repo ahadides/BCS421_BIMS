2b3ea3cafa54e2a5b9ba522f57262bdd9fec37d7
Elan Cheung <elan.cheung@gmail.com>
2023-11-26 16:47:47 -0500

    Pulled from main and added my changes

--- app/src/main/java/edu/farmingdale/bcs421_bims/RegistrationActivity.kt
+++ app/src/main/java/edu/farmingdale/bcs421_bims/RegistrationActivity.kt
@@ -1,104 +1,75 @@
 package edu.farmingdale.bcs421_bims
 
-
-import android.annotation.SuppressLint
-import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
 
 class RegistrationActivity : AppCompatActivity() {
-    private lateinit var firstN: EditText
-    private lateinit var lastN: EditText
-    private lateinit var DOB: EditText
+
+    private lateinit var auth: FirebaseAuth
+    private lateinit var firstNameEditText: EditText
+    private lateinit var lastNameEditText: EditText
+    private lateinit var dateOfBirthEditText: EditText
     private lateinit var emailEditText: EditText
-    private lateinit var passW: EditText
-    private lateinit var registerButton : Button
-    @SuppressLint("MissingInflatedId")
+    private lateinit var passwordEditText: EditText
+    private lateinit var registerButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registration)
-        firstN = findViewById(R.id.firstNameEditText)
-        lastN  = findViewById(R.id.familyNameEditText)
-        DOB = findViewById(R.id.dateOfBirthEditText)
+
+        //Initialize Firebase Auth
+        auth = FirebaseAuth.getInstance()
+
+        firstNameEditText = findViewById(R.id.firstNameEditText)
+        lastNameEditText = findViewById(R.id.familyNameEditText)
+        dateOfBirthEditText = findViewById(R.id.dateOfBirthEditText)
         emailEditText = findViewById(R.id.emailEditText)
-        passW = findViewById(R.id.editTextTextPassword2)
+        passwordEditText = findViewById(R.id.editTextTextPassword2)
         registerButton = findViewById(R.id.registerButton)
+
         registerButton.setOnClickListener {
-            val firstName = firstN.text.toString()
-            val lastName = lastN.text.toString()
-            val dateOfBirth = DOB.text.toString()
-            val email = emailEditText.text.toString()
-            val password = passW.text.toString()
-            if(isInputEmpty(firstName, lastName, dateOfBirth, email, password)) {
-                if (isValidInput(firstName, lastName, dateOfBirth, email, password)) {
-                    // Data is valid, you can proceed with registration
-                    val newUser = Users(firstName, lastName, dateOfBirth, email, password)
-                    newUser.firstName = firstName
-                    newUser.lastName = lastName
-                    newUser.dateOfBirth = dateOfBirth
-                    newUser.email = email
-                    newUser.password = password
-                    val intent = Intent(this, Login_Activity::class.java)
-                    // Add your registration logic here
-                    Log.d("NewUser", newUser.email)
-                    intent.putExtra("newUser", newUser)
-                    setResult(Activity.RESULT_OK, intent)
-                    Toast.makeText(this, "User Registered", Toast.LENGTH_SHORT).show()
-                    finish()
-                } else {
-                    Toast.makeText(this, "Error: Check Information Format ", Toast.LENGTH_SHORT).show()
+            val firstName = firstNameEditText.text.toString().trim()
+            val lastName = lastNameEditText.text.toString().trim()
+            val dateOfBirth = dateOfBirthEditText.text.toString().trim()
+            val email = emailEditText.text.toString().trim()
+            val password = passwordEditText.text.toString().trim()
 
-                    // Data is not valid, display an error message
-                }
-            }else{
-                Toast.makeText(this, "Error: Empty Fields", Toast.LENGTH_SHORT).show()
+            if (firstName.isNotEmpty() && lastName.isNotEmpty() && dateOfBirth.isNotEmpty() && email.isNotEmpty() && password.isNotEmpty()) {
+                //Perform registration using Firebase Auth
+                auth.createUserWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(this) { task ->
+                        if (task.isSuccessful) {
+                            //On successful authentication, save additional user info in the database
+                            val user = auth.currentUser
+                            val databaseRef = FirebaseDatabase.getInstance().getReference("users")
+                            val newUser = Users(firstName, lastName, dateOfBirth, email) // Assuming a User class with necessary fields
+                            user?.let {
+                                databaseRef.child(it.uid).setValue(newUser).addOnCompleteListener { task ->
+                                    if (task.isSuccessful) {
+                                        Toast.makeText(this, "Registration successful", Toast.LENGTH_SHORT).show()
+                                        // Redirect to login or main activity
+                                        val homeIntent = Intent(this, LoginActivity::class.java)
+                                        startActivity(homeIntent)
+                                        finish()
+                                    } else {
+                                        Toast.makeText(this, "Failed to save user info: ${task.exception?.message}", Toast.LENGTH_SHORT).show()
+                                    }
+                                }
+                            }
+                        } else {
+                            Toast.makeText(this, "Authentication failed: ${task.exception?.message}", Toast.LENGTH_SHORT).show()
+                        }
+                    }
+            } else {
+                Toast.makeText(this, "Please fill all fields", Toast.LENGTH_SHORT).show()
             }
-
         }
-
-    }
-
-
-
-    private fun isValidInput(firstName: String, familyName: String, dateOfBirth: String, email: String, password: String): Boolean {
-        // Perform validation here
-        return (isValidFirstName(firstName) && isValidFamilyName(familyName) && isValidDateOfBirth(dateOfBirth) && isValidEmail(email) && isValidPassword(password))
-    }
-
-    private fun isValidFirstName(firstName: String): Boolean {
-        return firstName.length in 3..30
-    }
-
-    private fun isValidFamilyName(familyName: String): Boolean {
-        return familyName.length in 3..30
-    }
-
-    private fun isValidDateOfBirth(dateOfBirth: String): Boolean {
-        //checking the format or the date's validity
-        return dateOfBirth.matches("\\d{4}\\d{2}\\d{2}".toRegex())
-    }
-
-    private fun isValidEmail(email: String): Boolean {
-        return android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()
-    }
-
-    private fun isValidPassword(password: String): Boolean {
-        // password validation rules
-        val passwordPattern = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).{8,}$".toRegex()
-        return passwordPattern.matches(password)
-    }
-    private fun isInputEmpty(firstName: String, lastName: String, dateOfBirth: String, email: String, password: String): Boolean {
-        // Check if any of the fields is empty
-        if (firstName.isEmpty() || lastName.isEmpty() || dateOfBirth.isEmpty() || email.isEmpty() || password.isEmpty()) {
-            return false
-        }
-
-        return true
     }
 }
 
